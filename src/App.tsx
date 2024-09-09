9954b671e5fe7a27debdef83c0c64d95fe96f5dc
aropsta16 <aropsta16@gmail.com>
2024-09-09 17:33:11 +1000

    dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import { Button, ButtonGroup } from "@chakra-ui/react";
-import viteLogo from "/vite.svg";
+import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
 import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: ` "nav" "main" `,
+        lg: `"nav nav" "aside main" `,
+      }}
+    >
+      <GridItem area="nav">
+        <NavBar />
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
