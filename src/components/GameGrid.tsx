c7666c49222cadc36aa7e1655beab38735c8e369
aropsta16 <aropsta16@gmail.com>
2024-09-10 19:19:47 +1000

    placeholders for when data is loading

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,19 +1,30 @@
 import { HStack, Image, SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
+import GameCardLoading from "./GameCardLoading";
+import CardContainer from "./CardContainer";
 
 const GameGrid = () => {
-  const { error, games } = useGames();
+  const { error, games, isLoading } = useGames();
+  const loadPlaceholder = [1, 2, 3, 4, 5, 6];
   return (
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
         spacing={10}
-        paddingBlock={10}
+        // paddingBlock={10}
       >
+        {isLoading &&
+          loadPlaceholder.map((pHolder) => (
+            <CardContainer>
+              <GameCardLoading key={pHolder} />
+            </CardContainer>
+          ))}
         {games.map((game) => (
-          <GameCard key={game.id} game={game}></GameCard>
+          <CardContainer>
+            <GameCard key={game.id} game={game}></GameCard>
+          </CardContainer>
         ))}
       </SimpleGrid>
     </>
