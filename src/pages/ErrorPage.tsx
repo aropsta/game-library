1f07db8af50d8ccb37d9ebb2fe8bb213817a925f
aropsta16 <aropsta16@gmail.com>
2024-09-20 02:11:50 +1000

    created error page

---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,21 @@
+import { Heading, Text } from "@chakra-ui/react";
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+
+import Navbar from "../components/NavBar";
+const ErrorPage = () => {
+  const error = useRouteError();
+  return (
+    <>
+      <Navbar />
+      <Heading>
+        <Text>
+          {isRouteErrorResponse(error)
+            ? "Invalid Page!"
+            : "An unexpected error has occured."}
+        </Text>
+      </Heading>
+    </>
+  );
+};
+
+export default ErrorPage;
